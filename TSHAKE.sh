73698bcb62751e01a11940e1e0d9a88a6d183ccd
TEAM TSHAKE <nadoo.ali.15.4@gmail.com>
2017-07-02 23:19:56 +0300

    Update TSHAKE.sh

--- TSHAKE.sh
+++ TSHAKE.sh
@@ -2,7 +2,8 @@
  COUNTER=0
 while [  $COUNTER -lt 5 ]; do
 kill $(pgrep telegram-cli)
-echo -e "\e[38;5;77m"   
+function print_logo() {
+	echo -e "\e[38;5;77m"   
 echo -e "       CH > @TshAkETEAM            "
 echo -e "       CH > @TshAkETEAM           "
 echo -e "       CH > @TshAkETEAM    "
@@ -11,14 +12,29 @@ echo -e "       CH > @TshAkETEAM          \e[38;5;88m"
 echo -e ""
 echo -e ""
 echo -e ""
-echo -e "        _____    _        _    _    _____    Dev @lIMyIl"
-echo -e "       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL"
-echo -e "         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI"
-echo -e "         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173"
-echo -e "         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl"
-echo -e "                                             Dev @h_k_a"
-sleep 1
+echo -e "\e[33m        _____    _        _    _    _____    Dev @lIMyIl\e[0m"
+echo -e "\e[33m       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL\e[0m"
+echo -e "\e[33m         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI\e[0m"
+echo -e "\e[33m         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173\e[0m"
+echo -e "\e[33m         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl\e[0m"
+echo -e "\e[33m                                             Dev @h_k_a\e[0m"
+}
+sleep 2
+if [ ! -f ./tg ]; then
 echo -e ""
+echo -e "\e[33m        _____    _        _    _    _____    Dev @lIMyIl"
+echo -e "\e[33m       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL"
+echo -e "\e[33m         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI"
+echo -e "\e[33m         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173"
+echo -e "\e[33m         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl"
+echo -e "\e[33m                                             Dev @h_k_a"
+    echo "\e[31;1mtg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+ sleep 2
+  print_logo
+   echo -e ""
 echo -e ""
 echo -e "        \e[38;5;300mOperation | Starting Bot"
 echo -e "        Source | TSHAKE Version 28 March 2017"
@@ -30,7 +46,7 @@ echo -e "        Dev | @H_173"
 echo -e "        Dev | @EMADOFFICAL"
 echo -e "        Dev | @h_k_a"
 echo -e "        \e[38;5;40m"
-sleep 2
-   ./tg -s ./TSHAKE.lua
+
+   ./tg -s ./TshAkE-cli.lua
 sleep 3
 done
